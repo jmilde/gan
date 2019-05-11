e1e2fa3277b4eab488382406a416ab0b88cc8148
jmilde <jan.milde@hotmail.de>
2019-05-11 22:55:06 +0200

    added working cadpvae

--- src/model_cadpvae.py
+++ src/model_cadpvae.py
@@ -15,9 +15,8 @@ def gan(data, btlnk_dim, data_dim, z_dim, dense_dim):
         x = normalize(tf.layers.dense(x, btlnk_dim, use_bias=False, activation=tf.nn.leaky_relu), "layer_norm_2")
         # output
         logits = tf.layers.dense(x, data_dim, use_bias=False)
-        return logits
+        return tf.nn.sigmoid(logits)
         #return tf.clip_by_value(x, 0.0, 1.0)
-
 
 
     def discriminator(x, cond, btlnk_dim, dense_dim, data_dim):
@@ -30,7 +29,9 @@ def gan(data, btlnk_dim, data_dim, z_dim, dense_dim):
         # bottleneck
         x = normalize(tf.layers.dense(x, btlnk_dim, use_bias=False, activation=tf.nn.leaky_relu),"layer_norm_2")
         logits = tf.layers.dense(x, data_dim, use_bias=False)
-        return logits
+        return tf.nn.sigmoid(logits)
+
+
 
     with tf.variable_scope("Input"):
         inpt = placeholder(tf.float32, [None, data_dim], data[0], "input")
@@ -51,17 +52,21 @@ def gan(data, btlnk_dim, data_dim, z_dim, dense_dim):
 
     with tf.variable_scope("loss"):
         with tf.variable_scope("d_loss"):
-            d_loss_real = tf.reduce_mean(
-                tf.nn.softmax_cross_entropy_with_logits_v2(labels=inpt, logits=y_real))
-            d_loss_fake = tf.reduce_mean(
-                tf.nn.softmax_cross_entropy_with_logits_v2(labels=x_fake, logits=y_fake))
+            d_loss_real = tf.reduce_mean(tf.losses.absolute_difference(inpt,y_real))
+            d_loss_fake = tf.reduce_mean(tf.losses.absolute_difference(x_fake, y_fake))
+            #d_loss_real = tf.reduce_mean(
+            #    tf.nn.softmax_cross_entropy_with_logits_v2(labels=inpt, logits=y_real))
+            #d_loss_fake = tf.reduce_mean(
+            #    tf.nn.softmax_cross_entropy_with_logits_v2(labels=x_fake, logits=y_fake))
             d_loss = d_loss_real - d_loss_fake
 
         with tf.variable_scope("g_loss"):
-            g_loss_g = tf.reduce_mean(
-                tf.nn.softmax_cross_entropy_with_logits_v2(labels=inpt, logits=x_fake))
-            g_loss_d = tf.reduce_mean(
-                tf.nn.softmax_cross_entropy_with_logits_v2(labels=x_fake, logits=y_fake))
+            g_loss_g = tf.reduce_mean(tf.losses.absolute_difference(inpt, x_fake))
+            g_loss_d = tf.reduce_mean(tf.losses.absolute_difference(x_fake, y_fake))
+            #g_loss_g = tf.reduce_mean(
+            #    tf.nn.softmax_cross_entropy_with_logits_v2(labels=inpt, logits=x_fake))
+            #g_loss_d = tf.reduce_mean(
+            #    tf.nn.softmax_cross_entropy_with_logits_v2(labels=x_fake, logits=y_fake))
             g_loss = g_loss_g + g_loss_d
 
 
@@ -85,4 +90,5 @@ def gan(data, btlnk_dim, data_dim, z_dim, dense_dim):
                 g_step=g_step,
                 x_fake=x_fake,
                 z_inpt=z_inpt,
+                y_fake=y_fake,
                 y_real=y_real)
--- src/train_cadpvae.py
+++ src/train_cadpvae.py
@@ -3,17 +3,21 @@ from util_io import pform
 from util_np import np
 from model_cadpvae import gan
 from tqdm import tqdm
+from numpy.random import RandomState
 
 #load data
+test_size = 900
 (train_images, train_labels),(test_images, test_labels) = tf.keras.datasets.mnist.load_data()
-x_train = np.reshape(np.concatenate([train_images, test_images]), (70000, 28*28))/255
-y_train = np.concatenate([train_labels, test_labels])
+x_train = np.reshape(np.concatenate([train_images, test_images[test_size:]]), (70000-test_size, 28*28))/255
+y_train = np.concatenate([train_labels, test_labels[test_size:]])
+x_test = np.reshape(test_images[:test_size],(test_size,28*28))/255
+y_test= np.reshape(test_labels[:test_size],(test_size,1))
 
 path_log = "../logs/"
 path_ckpt = "../ckpt/"
 
 
-trial = "mnist_cadpvae"
+trial = "mnist_cadpvae3"
 epochs = 200
 batch_size = 500
 
@@ -22,7 +26,7 @@ dense_dim = [64] # add numbers to create more layers, g and d are symatrical
 btlnk_dim = 4
 data_dim = len(x_train[0])
 
-
+rand = RandomState(0) #fix seed
 # data pipeline
 batch_fn = lambda: batch3(x_train, y_train, batch_size, z_dim)
 data = pipe(batch_fn, (tf.float32, tf.float32, tf.float32), prefetch=4)
@@ -42,18 +46,17 @@ saver = tf.train.Saver()
 tf.global_variables_initializer().run()
 
 # tensorboard summary
-smry = tf.summary.merge([tf.summary.scalar('gnrt_loss', model['g_loss']),
-                         tf.summary.scalar('dscr_loss', model['d_loss']),
-                         tf.summary.image('img_fake', tf.reshape(model['x_fake'], [-1,28,28,1]), max_outputs=1),
-                         tf.summary.image('img_inpt', tf.reshape(model['inpt'], [-1,28,28,1]), max_outputs=1),
-                         tf.summary.image('imgs_fake', tf.reshape(
-                             tf.transpose(
-                                 tf.reshape(
-                                     sess.run(model["x_fake"],
-                                              {model["cond"]:np.random.randint(0,9,(900,1)),
-                                               model["z_inpt"]:np.random.normal(size=(900,z_dim))}),
-                                     (30,30,28,28)),(0,2,1,3)),
-                             (1,30*28,30*28, 1)))])
+smry = tf.summary.merge([tf.summary.scalar('g_loss', model['g_loss']),
+                         tf.summary.scalar('d_loss', model['d_loss']),
+                         tf.summary.image('img_g', tf.reshape(model['x_fake'], [-1,28,28,1]), max_outputs=1),
+                         tf.summary.image('img_d_g', tf.reshape(model['y_fake'], [-1,28,28,1]), max_outputs=1),
+                         tf.summary.image('img_d_r', tf.reshape(model['y_real'], [-1,28,28,1]), max_outputs=1),
+                         tf.summary.image('imgs_g', tf.reshape(
+                             tf.transpose(tf.reshape(model["x_fake"],(30,30,28,28)),(0,2,1,3)),(1,30*28,30*28, 1))),
+                         tf.summary.image('imgs_d_g', tf.reshape(
+                             tf.transpose(tf.reshape(model["y_fake"],(30,30,28,28)),(0,2,1,3)),(1,30*28,30*28, 1))),
+                        tf.summary.image('imgs_d_r', tf.reshape(
+                             tf.transpose(tf.reshape(model["y_real"],(30,30,28,28)),(0,2,1,3)),(1,30*28,30*28, 1)))])
 
 
 wrtr = tf.summary.FileWriter(pform(path_log, trial))
@@ -67,5 +70,7 @@ for epoch in tqdm(range(epochs)):
         sess.run(model['g_step'])
     # tensorboard writer
     step = sess.run(model['step'])
-    wrtr.add_summary(sess.run(smry, {model["z_inpt"]:np.random.normal(size= (30*30, z_dim))}), step)
+    wrtr.add_summary(sess.run(smry, {model["inpt"]:x_test,
+                                     model["cond"]:y_test,
+                                     model["z_inpt"]:rand.normal(size=(900,z_dim),)}), step)
     wrtr.flush()
