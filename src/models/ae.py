3ca33601490e1628f0b4d84204ae71ec0d2e3477
jmilde <jan.milde@hotmail.de>
2019-06-27 11:18:36 +0200

    added ae

---
+++ src/models/ae.py
@@ -0,0 +1,48 @@
+try:
+    from util_tf import tf, placeholder, normalize
+    from util_np import np
+except ImportError:
+    from src.util_tf import tf, placeholder, normalize
+    from src.util_np import np
+
+def ae(data, btlnk_dim, data_dim, dense_dim, y_dim):
+
+    def encoder(x, btlnk_dim):
+        x = normalize(tf.nn.relu(tf.keras.layers.Dense(btlnk_dim, use_bias=False)(x)), "layer_norm_1")
+        return x
+
+    def decoder(x, data_dim):
+        x = tf.keras.layers.Dense(data_dim, use_bias=False)(x)
+        return x
+
+    with tf.variable_scope("x"):
+        x = placeholder(tf.float32, [None, data_dim], data[0], "x")
+    with tf.variable_scope("y"):
+        y = placeholder(tf.float32, [None], data[1], "y")
+
+    with tf.variable_scope("encoder"):
+        z = encoder(x, btlnk_dim)
+
+    with tf.variable_scope("decoder"):
+        logits = decoder(z, data_dim)
+
+    with tf.variable_scope("loss"):
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=x, logits=logits))
+
+    step = tf.train.get_or_create_global_step()
+
+    with tf.variable_scope("AUC"):
+        anomaly_score = tf.reduce_mean((x-logits)**2, axis=1)
+        auc, _ = tf.metrics.auc(y, anomaly_score)
+
+    with tf.variable_scope("train_step"):
+        train_step = tf.train.AdamOptimizer().minimize(loss, step)
+
+
+    return dict(step=step,
+                x=x,
+                y=y,
+                logits=logits,
+                auc=auc,
+                train_step=train_step,
+                loss=loss)
---
+++ src/train_mnist_ae.py
@@ -0,0 +1,92 @@
+try:
+    from src.util_tf import tf, pipe, batch2, placeholder, profile
+    from src.util_io import pform
+    from src.util_np import np, unison_shfl
+    from src.models.ae import ae
+except ImportError:
+    from util_tf import tf, pipe, batch2, placeholder, profile
+    from util_io import pform
+    from util_np import np, unison_shfl
+    from models.ae import ae
+from tqdm import tqdm
+from numpy.random import RandomState
+
+
+def train():
+    #load data
+    anomaly_class = 8
+    (train_images, train_labels),(test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+    inlier = train_images[train_labels!=anomaly_class]
+    x_train = np.reshape(inlier, (len(inlier), 28*28))/255
+    y_train = train_labels[train_labels!=anomaly_class]
+    outlier = train_images[train_labels==anomaly_class]
+    x_test = np.reshape(np.concatenate([outlier, test_images])
+                        ,(len(outlier)+len(test_images), 28*28))/255
+    y_test= np.concatenate([train_labels[train_labels==anomaly_class], test_labels])
+    y_test = [0 if y!=anomaly_class else 1 for y in y_test]
+    x_test, y_test = unison_shfl(x_test, np.array(y_test))
+    path_log = "/cache/tensorboard-logdir/ae"
+    path_ckpt = "/project/outlier_detection/ckpt"
+
+    epochs = 200
+    batch_size = 64
+
+    dense_dim = 64
+    btlnk_dim = 32
+    y_dim = 1
+    data_dim = len(x_train[0])
+    trial = f"{anomaly_class}dae_b{batch_size}_d{dense_dim}_btlnk{btlnk_dim}"
+
+
+    rand = RandomState(0) #fix seed
+    # data pipeline
+    batch_fn = lambda: batch2(x_train, y_train, batch_size)
+    data = pipe(batch_fn, (tf.float32, tf.float32), prefetch=4)
+    #z = tf.random_normal((batch_size, z_dim))
+
+    # load graph
+    model = ae(data, btlnk_dim, data_dim, dense_dim, y_dim)
+
+    # start session, initialize variables
+    sess = tf.InteractiveSession()
+    saver = tf.train.Saver()
+
+
+    wrtr = tf.summary.FileWriter(pform(path_log, trial))
+    #wrtr.add_graph(sess.graph)
+
+    # tensorboard summary
+    auc = placeholder(tf.float32, (), name="AUC")
+
+    ### if load pretrained model
+    # pretrain = "modelname"
+    #saver.restore(sess, pform(path_ckpt, pretrain))
+    ### else:
+    init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+    sess.run(init)
+
+    def log(step
+            , wrtr= wrtr
+            , log = tf.summary.merge([tf.summary.scalar('loss', model['loss'])
+                                      , tf.summary.image('img', tf.reshape(model['logits'], [-1,28,28,1]), max_outputs=1)
+                                      #, tf.summary.image('dx900', tf.reshape(
+                                      #    tf.transpose(tf.reshape(model["dx"][:900],(30,30,28,28)),(0,2,1,3)),(1,30*28,30*28, 1)))
+                                      , tf.summary.scalar("AUC", model["auc"])])
+            , y= y_test
+            , x= x_test):
+        wrtr.add_summary(sess.run(log), step)
+        wrtr.flush()
+
+
+
+    for epoch in tqdm(range(epochs)):
+        for i in range(len(x_train)//batch_size):
+            sess.run(model['train_step'])
+        # tensorboard writer
+        log(sess.run(model["step"]))
+
+    saver.save(sess, pform(path_ckpt, trial), write_meta_graph=False)
+
+
+if __name__ == "__main__":
+    train()
